cd5b27b4f472b4a7143b2a4c8511312e8c549693
GokilaJeyaram <62006133+GokilaJeyaram@users.noreply.github.com>
2020-04-05 19:20:46 +0530

    odd or even

---
+++ OddEven.java
@@ -0,0 +1,20 @@
+package week1.day6;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		int num = 878;
+		int a;
+		a = num%2;
+		if(a==0)
+		{
+			System.out.println("The given number is even: "+num);
+		}
+		else
+		{
+			System.out.println("The given number is odd: "+num);
+		}
+
+	}
+
+}
